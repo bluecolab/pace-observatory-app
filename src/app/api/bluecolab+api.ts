6b2d1b300a76baa77c5db406ace87023457a9b5c
Kenji Okura <okurakeng@gmail.com>
2026-04-25 15:02:18 -0400

    ???

--- src/app/api/bluecolab+api.ts
+++ src/app/api/bluecolab+api.ts
@@ -1,5 +1,11 @@
 export async function POST(request: Request) {
     const { request: url } = await request.json();
+    const urlObj = URL.parse(url);
+
+    if (!urlObj || urlObj.hostname !== 'colabprod01.pace.edu') {
+        return new Response('Invalid API endpoint', { status: 400 });
+    }
+
     const response = await fetch(url);
     return Response.json(await response.json());
 }
