93b2017905d5bc5633e82e162837fe3dbfe8331b
Kenji Okura <okurakeng@gmail.com>
2026-04-27 11:48:16 -0400

    AWM-143 - Fix failing tests (#389)

--- src/app/api/purpleair+api.ts
+++ src/app/api/purpleair+api.ts
@@ -1,5 +1,5 @@
-const API_URL = 'https://api.purpleair.com/v1/sensors';
-const API_TIMEOUT = 10000; // 10 seconds
+// const API_URL = 'https://api.purpleair.com/v1/sensors';
+// const API_TIMEOUT = 10000; // 10 seconds
 
 export async function GET() {
     return {};
